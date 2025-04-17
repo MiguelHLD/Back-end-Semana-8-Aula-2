34c45e9e163e02503cbf83ffc6f0b0d762e6ed48
MiguelHLD <miguelhld447@gmail.com>
2025-04-17 15:20:08 -0300

    Mensagem explicando a alteração

---
+++ ecommerce_microsservicos/product_service.py
@@ -0,0 +1,8 @@
+class ProductService:
+    def __init__(self):
+        self.products = []
+        def add_product(self, product):
+            self.products.append(product)
+            def check_availability(self, product):
+                return product in self.products
+            
